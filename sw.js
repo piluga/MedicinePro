78ed7bfc6167f3861cac906f077a69d66c42f32b
piluga <133677361+piluga@users.noreply.github.com>
2026-02-26 17:24:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'medicinepro-cache-v19';
+const CACHE_NAME = 'medicinepro-cache-v20';
 
 // 1. Risorse Locali
 const localUrls = [
@@ -99,6 +99,7 @@ self.addEventListener('fetch', event => {
         })
     );
 });
+
 
 
 
